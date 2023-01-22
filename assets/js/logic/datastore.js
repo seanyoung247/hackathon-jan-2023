2dc1d211c77f781331e1f8b820c60e8871cf2d3c
sean <seanyoung247@gmail.com>
2023-01-22 14:23:45 +0000

    feat: Adds income and expenses calculations

--- assets/js/logic/datastore.js
+++ assets/js/logic/datastore.js
@@ -51,9 +51,33 @@ export class DataStore {
         return fromWeekly(total, frequency);
     }
 
-    getTotalIncome(frequency='weekly') {}
-    getTotalExpenses(frequency='weekly') {}
-    getDisposableIncome(frequency='weekly') {}
+    getTotalIncome(frequency='weekly') {
+        let total = 0;
+        for (const [key,category] of Object.entries(this._categories)) {
+            // Only count income categories
+            if (category.income) {
+                total += this.getCategoryTotal(key, frequency);
+            }
+            return total;
+        }
+    }
+
+    getTotalExpenses(frequency='weekly') {
+        let total = 0;
+        for (const [key,category] of Object.entries(this._categories)) {
+            // Only count expense categories
+            if (!category.income) {
+                total += this.getCategoryTotal(key, frequency);
+            }
+            return total;
+        }
+    }
+
+    getDisposableIncome(frequency='weekly') {
+        const income = this.getTotalIncome(frequency);
+        const expenses = this.getTotalExpenses(frequency);
+        return income - expenses;
+    }
 
 
     /* TODO:
@@ -67,10 +91,10 @@ export class DataStore {
 
 // Category total (at the bottom of the modal). Total income, total expenses, and remaining income after expenses
 const factor = {
-    'weekly': 1,
-    'monthly': 4.33,
-    'quarterly': 13,
-    'yearly': 52
+    'weekly': 1,        // One week in a week
+    'monthly': 4.33,    // Average of 4.33 weeks in a month
+    'quarterly': 13,    // 13 weeks in a quarter
+    'yearly': 52        // 52 weeks in a year
 }
 
 function toWeekly(value, frequency) {
