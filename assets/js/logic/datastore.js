34384cf39a8fdca3de80f0d8b4fc6db79505c678
Russ-S <118801685+rstan-dev@users.noreply.github.com>
2023-01-22 15:42:27 +0000

    merging from main (#21)
    * merge main into landing (#17)
    
    * Added header image, background and hero image to landing page
    
    * feat: Adds moneyinput functionality
    
    * feat: Adds moneyfield events
    
    * fix: fixes api exports
    
    * Hero-branch pull request (#14)
    
    * Hero image (#11)
    
    * CSS: increase income section
    
    * Added header image, background and hero image to landing page
    
    Co-authored-by: Russ-S <118801685+rstan-dev@users.noreply.github.com>
    Co-authored-by: Russ-S <rstanleym74@gmail.com>
    
    * Components (#12)
    
    * feat: Adds moneyinput functionality
    
    * feat: Adds moneyfield events
    
    * fix: fixes api exports
    
    Co-authored-by: Russ-S <118801685+rstan-dev@users.noreply.github.com>
    Co-authored-by: Russ-S <rstanleym74@gmail.com>
    Co-authored-by: Sean Young <seanyoung247@gmail.com>
    
    * feat: Adds invalid field highlighting
    
    * Added icom button to index.html
    
    * feat: Adds frequency cost converter
    
    * Added media queries to income button
    
    * feat: Adds add input and back buttons
    
    * Duplicate buttons for home page
    
    * changed style for button borders
    
    * Add icon to entertainment button
    
    * feat: Adds basic input modal styling
    
    * feat: Adds initial category values
    
    * update button ids
    
    * feat: Adds initial data store class
    
    * feat: Adds categories data
    
    * refactor: Simplifies category creation code
    
    * feat: Adds field deletion and enumeration
    
    * feat: Adds event bubbling to moneyInput
    
    * feat: Adds field loading and saving
    
    * feat: Adds field removal
    
    * refactor: TODO list
    
    * removed individual modals from button
    
    * refactor: Removes stale code
    
    Co-authored-by: rhysmoggs <rhysmoggs@hotmail.com>
    Co-authored-by: sean <seanyoung247@gmail.com>
    Co-authored-by: Rhys <92308810+rhysmoggs@users.noreply.github.com>
    
    * Components - DataStore (#19)
    
    * chore: Merge fixes
    
    * feat: Adds modal triggers to all categories
    
    * feat: Adds static property to Modal
    A static modal doesn't close when clicking on the background.
    
    * feat: Adds category total calculation
    
    * feat: Adds income and expenses calculations
    
    * refactor: Removes stale todo
    
    * fix: Fixes datastore not being updated on field deletion
    
    * changed button layout
    
    Co-authored-by: Sean Young <seanyoung247@gmail.com>
    Co-authored-by: rhysmoggs <rhysmoggs@hotmail.com>
    Co-authored-by: Rhys <92308810+rhysmoggs@users.noreply.github.com>

--- assets/js/logic/datastore.js
+++ assets/js/logic/datastore.js
@@ -43,12 +43,61 @@ export class DataStore {
     /* Persistent storage */
 
     /* Value calculations */
+    getCategoryTotal(category, frequency='weekly') {
+        let total = 0;
+        for (const field of Object.values(this._categories[category].fields)) {
+            total += toWeekly(field.cost, field.freq);
+        }
+        return fromWeekly(total, frequency);
+    }
+
+    getTotalIncome(frequency='weekly') {
+        let total = 0;
+        for (const [key,category] of Object.entries(this._categories)) {
+            // Only count income categories
+            if (category.income) {
+                total += this.getCategoryTotal(key, frequency);
+            }
+            return total;
+        }
+    }
+
+    getTotalExpenses(frequency='weekly') {
+        let total = 0;
+        for (const [key,category] of Object.entries(this._categories)) {
+            // Only count expense categories
+            if (!category.income) {
+                total += this.getCategoryTotal(key, frequency);
+            }
+            return total;
+        }
+    }
+
+    getDisposableIncome(frequency='weekly') {
+        const income = this.getTotalIncome(frequency);
+        const expenses = this.getTotalExpenses(frequency);
+        return income - expenses;
+    }
 
     /* TODO:
         - Local storage saving and loading values
-        - Value calculations
-          - Total income
-          - Total Expenditure
-          - Category totals
     */
+}
+
+// Category total (at the bottom of the modal). Total income, total expenses, and remaining income after expenses
+const factor = {
+    'weekly': 1,        // One week in a week
+    'monthly': 4.33,    // Average of 4.33 weeks in a month
+    'quarterly': 13,    // 13 weeks in a quarter
+    'yearly': 52        // 52 weeks in a year
+}
+
+function toWeekly(value, frequency) {
+    if (value) return value / factor[frequency];
+    return 0;
+}
+
+function fromWeekly(value, frequency) {
+    if (value) return value * factor[frequency];
+    return 0;
 }
